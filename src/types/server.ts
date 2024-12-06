daecb7eccd010d01833d27ef43a6dd8156cd349d
bbbjihan <74421125+bbbjihan@users.noreply.github.com>
2024-12-07 03:44:57 +0900

    feat: server dto interface 선언

--- src/types/server.ts
+++ src/types/server.ts
@@ -12,3 +12,44 @@ export interface Comment {
   email: string;
   body: string;
 }
+
+/**
+ * Message
+ */
+interface Message {
+  userName: string;
+  text: string;
+  imageUrl: string;
+}
+
+/**
+ * Capsule
+ */
+type Coordinate = [number, number];
+
+interface Map {
+  coordinate: Coordinate;
+}
+interface CapsuleBase {
+  capsuleDesignId: number;
+  title: string;
+  map: Map;
+  goalTime: number;
+  messageCount: number;
+}
+
+interface UndiggedCapsule extends CapsuleBase {
+  status: "undigged";
+}
+
+interface DiggedCapsule extends CapsuleBase {
+  status: "digged";
+  createTime: number;
+}
+
+interface ClosedCapsule extends CapsuleBase {
+  status: "opened";
+  message: Message[];
+}
+
+export type Capsule = UndiggedCapsule | DiggedCapsule | ClosedCapsule;
