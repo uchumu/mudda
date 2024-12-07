aef6e0f9eff4688029b643dcf9817be8bca853e8
bunzzeok <bunzzeok@naver.com>
2024-12-07 13:15:44 +0900

    feat: update default height and max-width
    feat: update default height and max-width

--- src/_app/index.tsx
+++ src/_app/index.tsx
@@ -2,4 +2,17 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Providers from "./Providers";
 
+const updateViewportHeight = () => {
+  // 현재 화면의 실제 높이를 1vh 단위로 계산
+  const vh = window.innerHeight * 0.01;
+  // CSS 변수에 --vh로 저장
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+
+// 초기 실행
+updateViewportHeight();
+
+// 화면 크기 변화 시 다시 계산
+window.addEventListener("resize", updateViewportHeight);
+
 createRoot(document.getElementById("root")!).render(<Providers />);
