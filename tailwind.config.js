1b95f628c15e009dc63ce34368f3cece660603ed
bbbjihan <74421125+bbbjihan@users.noreply.github.com>
2024-12-07 15:24:43 +0900

    config: tailwind color palette 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        "primary-main": "#5194F9",
+        "primary-text": "#FFFFFF",
+        "primary-disabled": "#A1A1A1",
+        "primary-paper": "#F6FAFF",
+      },
+    },
   },
   plugins: [],
 };
