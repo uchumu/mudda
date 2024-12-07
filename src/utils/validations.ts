ac42bdff009e969e30c0618c4603ba1081c6172a
bbbjihan <74421125+bbbjihan@users.noreply.github.com>
2024-12-07 19:46:43 +0900

    fix: unused var error resolve

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -3,5 +3,7 @@
  * valid할 경우 True를, invalid할 경우 False를 반환하는 함수입니다.
  */
 
-export const getInputCapsuleNameValid = (name: string) => true;
-export const getInputCapsulePasswordValid = (password: string) => true;
+export const getInputCapsuleNameValid = (name: string) =>
+  typeof name === "string";
+export const getInputCapsulePasswordValid = (password: string) =>
+  typeof password === "string";
