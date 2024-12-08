0e3801e823006272b2872d4c6e62cde9d4087b16
bbbjihan <74421125+bbbjihan@users.noreply.github.com>
2024-12-08 09:03:40 +0900

    remove: console.log

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -12,7 +12,6 @@ export default function PickerAccordion({ selected, setSelected }: Props) {
   const isSelectedPicker = selected ? `${selected?.toLocaleDateString()}` : "";
 
   const formatTime = useCallback((date: Date) => {
-    console.log(date,"format")
     const hours = date.getHours().toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
     const seconds = date.getSeconds().toString().padStart(2, "0");
@@ -21,8 +20,7 @@ export default function PickerAccordion({ selected, setSelected }: Props) {
 
   const handleTimeChange = useCallback(
     (hours: number, minutes: number, seconds: number) => {
-      console.log('')
-      setSelected((prev:Date) => {
+      setSelected((prev: Date) => {
         const newDate = new Date(prev);
         newDate.setHours(hours);
         newDate.setMinutes(minutes);
