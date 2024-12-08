dd84329f8c9685e21b5e20ef5a5b58c23bc80977
bunzzeok <bunzzeok@naver.com>
2024-12-08 15:22:19 +0900

    fix : update date pick page scroll
    fix : update date pick page scroll

--- src/_app/pages/CreateCapsulePage/Steps/SelectDateStep/index.tsx
+++ src/_app/pages/CreateCapsulePage/Steps/SelectDateStep/index.tsx
@@ -4,18 +4,18 @@ import Margin from "@/components/Margin";
 import { Dispatch, SetStateAction } from "react";
 
 type Props = {
-    selectDate : Date;
-    setSelectDate : Dispatch<SetStateAction<Date>>;
-}
-const SelectDateStep = ({selectDate, setSelectDate}:Props) => {
-    return (
-  <div className="flex-col px-[22px]">
-    <Margin H="54px" />
-    <StepHeader text={`캡슐을 \n$오픈할일시$를 선택해 주세요.`} />
-    <Margin H="54px" />
-    <PickerAccordion selected={selectDate} setSelected={setSelectDate}/>
-  </div>
-    )
+  selectDate: Date;
+  setSelectDate: Dispatch<SetStateAction<Date>>;
+};
+const SelectDateStep = ({ selectDate, setSelectDate }: Props) => {
+  return (
+    <div className="flex-col px-[22px] overflow-auto h-[calc(100vh-58px-56px-30px)]">
+      <Margin H="54px" />
+      <StepHeader text={`캡슐을 \n$오픈할일시$를 선택해 주세요.`} />
+      <Margin H="54px" />
+      <PickerAccordion selected={selectDate} setSelected={setSelectDate} />
+    </div>
+  );
 };
 
 export default SelectDateStep;
