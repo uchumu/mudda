3023196fd90d29494954306d8c958f7b5e8ca926
Minsoek96 <FE.minseok96@gmail.com>
2024-12-11 01:53:58 +0900

    fix : prevent screen transition from queryKey invalidation

--- src/queries/Capsule/useCapsuleService.ts
+++ src/queries/Capsule/useCapsuleService.ts
@@ -20,14 +20,25 @@ interface UseDigMutateProps {
 export const useDigMutate = ({ code }: UseDigMutateProps) => {
   const queryClient = useQueryClient();
 
-  return useMutation(
+  const mutation = useMutation(
     {
       mutationFn: CapsuleService.putCapsuleDig,
-      onSuccess: () =>
-        queryClient.invalidateQueries({
-          queryKey: queryKeys.capsule({ code }),
-        }),
+      onSuccess: () => {},
     },
     queryClient
   );
+
+  return {
+    ...mutation,
+    invalidateCapsuleQueries: () =>
+      queryClient.invalidateQueries({
+        queryKey: queryKeys.capsule({ code }),
+      }),
+  };
 };
+
+// 모달 확인 하기전에 캐싱이 초기화되서 페이지 전환되어 디자인 규격 안맞는 문제
+// onSuccess: () =>
+//   queryClient.invalidateQueries({
+//     queryKey: queryKeys.capsule({ code }),
+//   }),
